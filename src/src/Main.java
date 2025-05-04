2ef34bc1b561ff7fdb7ce564d21b764ed53bc00e
SergioTheS <202002456@estudantes.ips.pt>
2025-05-04 21:26:53 +0100

    Done

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,86 @@
 package src;
-
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Menu menu =  new Menu();
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the Number Guessing Game!");
 		System.out.println("I'm thinking of a number between 1 and 100.");
-		System.out.println("You have 5 chances to guess the correct number.");
 		int numToGuess = (int)(Math.random()*101);
 		menu.printMenu();
-		System.out.println(menu.getOption());
+		System.out.print("Please enter your guess: ");
+		int guess = sc.nextInt();
+		int count = 1;
+		int numberOfAttempts = 10;
+		switch(menu.getOption()) {
+		case 1:
+			numberOfAttempts = 10;
+			while(count < numberOfAttempts && guess != numToGuess) {
+					if(numToGuess < guess) {
+						System.out.println("Incorrect! The number is less than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}else {
+						System.out.println("Incorrect! The number is more than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}
+				count++;
+			}
+			if(guess != numToGuess) {
+				System.out.println("Oh no! You couldn't guess the number.\n");
+				System.out.println("The number was: "+ numToGuess);
+				
+			}else {
+				System.out.println("Congratulations! You guessed the correct number in "+ count +" attempts.");
+			}
+			break;
+		case 2:
+			numberOfAttempts = 5;
+			while(count < numberOfAttempts && guess != numToGuess) {
+					if(numToGuess < guess) {
+						System.out.println("Incorrect! The number is less than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}else {
+						System.out.println("Incorrect! The number is more than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}
+				count++;
+			}
+			if(guess != numToGuess) {
+				System.out.println("Oh no! You couldn't guess the number.\n");
+				System.out.println("The number was: "+ numToGuess);
+				
+			}else {
+				System.out.println("Congratulations! You guessed the correct number in "+ count +" attempts.");
+			}
+			break;
+		case 3:
+			numberOfAttempts = 3;
+			while(count < numberOfAttempts && guess != numToGuess) {
+					if(numToGuess < guess) {
+						System.out.println("Incorrect! The number is less than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}else {
+						System.out.println("Incorrect! The number is more than "+ guess + ".\n");
+						System.out.print("Please enter your guess: ");
+						guess = sc.nextInt();
+					}
+				count++;
+			}
+			if(guess != numToGuess) {
+				System.out.println("Oh no! You couldn't guess the number.\n");
+				System.out.println("The number was: "+ numToGuess);
+				
+			}else {
+				System.out.println("Congratulations! You guessed the correct number in "+ count +" attempts.");
+			}
+			break;
+		}
 		System.out.println(numToGuess);
 	}
 	
--- src/src/Menu.java
+++ src/src/Menu.java
@@ -23,7 +23,7 @@ public class Menu {
 		System.out.println("Please select the difficulty level:");
 		System.out.println("1. Easy (10 chances)");
 		System.out.println("2. Medium (5 chances)");
-		System.out.println("3. Hard (3 chances)");
+		System.out.println("3. Hard (3 chances)\n");
 		System.out.print("Please enter your choice: ");
 		option = scanner.nextInt();
 		while(option < 1 || option > 3) {
@@ -32,14 +32,19 @@ public class Menu {
 		}
 		switch(option) {
 		case 1:
-			System.out.println("Great! You have selected the Easy difficulty level.");
+			System.out.println("\nGreat! You have selected the Easy difficulty level.");
+			System.out.println("You have 10 chances to guess the correct number.\n");
 			break;
 		case 2:
-			System.out.println("Great! You have selected the Medium difficulty level.");
+			System.out.println("\nGreat! You have selected the Medium difficulty level.");
+			System.out.println("You have 5 chances to guess the correct number.\n");
 			break;
 		case 3:
-			System.out.println("Great! You have selected the Hard difficulty level.");
+			System.out.println("\nGreat! You have selected the Hard difficulty level.");
+			System.out.println("You have 3 chances to guess the correct number.\n");
 			break;
 		}
+		System.out.println("Let's start the game!\n");
 	}
+	
 }
